570c26e4b8fdcabcc1708c5e1dba25a616ed430e
jiangyushen66 <627596827@qq.com>
2021-12-28 22:55:10 +0800

    3项目的基本优化

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 const Koa = require('koa')
 
+const {APP_PORT} = require('./config/config.default')
+
 const app = new Koa()
 // 创建一个中间件
 app.use((ctx,next) =>{
-    ctx.body = 'hello koa'
+    ctx.body = 'hello Vue.js'
 })
 
-app.listen(3000,()=>{
-    console.log('the server is running on http://localhost:3000')
+console.log(APP_PORT)
+app.listen(APP_PORT,()=>{
+    console.log(`the server is running on http://localhost:${APP_PORT}`)
 })
